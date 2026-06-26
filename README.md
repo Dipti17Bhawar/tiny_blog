1eca18f5fca6212bcaec98678d2fd46c507ab40c
Dipti17Bhawar <diptibhawar@gmail.com>
2026-06-26 09:49:23 +0530

    Update project files

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Tiny-Blog 
+# Tiny-Blog   (https://tiny-blog-ui-r3v0.onrender.com/)
 
 This is Hobby Project to undersatnd how blog applications works 
---
+++ server/models/Blog.js
@@ -0,0 +1,19 @@
+import { model,Schema } from "mongoose";
+
+const blogSchema = new Schema ({
+    title:{type:String,required:true},
+    content:{type:String,required:true},
+    status:{type:String, default:"draft", enum:[ "draft", "published","archieved"],},
+    category:{type:String,required:true},
+    publishedAt:{type:Date},
+    author:{type:Schema.Types.ObjectId , ref :"User", required:true },
+},
+{
+    timestamps:true,
+}
+);
+
+
+const Blog = model("Blog",blogSchema);
+
+export default Blog;
